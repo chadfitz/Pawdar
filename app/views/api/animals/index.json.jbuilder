51b6dcece96069993da17b9af974a4068208b789
chad fitzgerald <fitzchad@gmail.com>
2023-01-03 21:27:22 -0800

    Update index json.jbuilder, add photos to all animal seeds

--- app/views/api/animals/index.json.jbuilder
+++ app/views/api/animals/index.json.jbuilder
@@ -1,6 +1,11 @@
-json.array! @animals do |animal|
-  json.set! animal.id do
-    json.extract! animal, :species, :breed, :size, :gender, :age, :color, :coat, :status, :name, :description, :organization_id
-  end
-  # json.photoUrl url_for(animal.photo)
+# json.array! @animals do |animal|
+#   json.set! animal.id do
+#     json.extract! animal, :species, :breed, :size, :gender, :age, :color, :coat, :status, :name, :description, :organization_id
+#   end
+#   # json.photoUrl url_for(animal.photo)
+# end
+
+json.array! @posts do |post|
+  json.extract! post, :id, :title
+  json.photoUrl url_for(post.photo)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -98,7 +98,7 @@ ApplicationRecord.transaction do
   #     organization_id: rand(1..5)
   #   })
   # end
-  Animal.create!({
+  cat_1 = Animal.create!({
     species: "Cat",
     breed: "Dilute Calico & Domestic Short Hair Mix",
     size: "Medium",
@@ -112,7 +112,7 @@ ApplicationRecord.transaction do
     organization_id: 1
   })
 
-  Animal.create!({
+  cat_2 = Animal.create!({
     species: "Cat",
     breed: "Tabby",
     size: "Medium",
@@ -126,7 +126,7 @@ ApplicationRecord.transaction do
     organization_id: 1
   })
 
-  Animal.create!({
+  cat_3 = Animal.create!({
     species: "Cat",
     breed: "Dilute Tortoiseshell & Domestic Short Hair Mix",
     size: "Small",
@@ -140,7 +140,7 @@ ApplicationRecord.transaction do
     organization_id: 1
   })
 
-  Animal.create!({
+  cat_4 = Animal.create!({
     species: "Cat",
     breed: "Domestic Medium Hair",
     size: "Small",
@@ -154,7 +154,7 @@ ApplicationRecord.transaction do
     organization_id: 1
   })
 
-  Animal.create!({
+  cat_5 = Animal.create!({
     species: "Cat",
     breed: "Domestic Short Hair",
     size: "Medium",
@@ -167,7 +167,7 @@ ApplicationRecord.transaction do
     organization_id: 2
   })
 
-  Animal.create!({
+  cat_6 = Animal.create!({
     species: "Cat",
     breed: "Domestic Short Hair",
     size: "Medium",
@@ -180,7 +180,7 @@ ApplicationRecord.transaction do
     organization_id: 2
   })
 
-  Animal.create!({
+  cat_7 = Animal.create!({
     species: "Cat",
     breed: "Domestic Short Hair",
     size: "Medium",
@@ -193,7 +193,7 @@ ApplicationRecord.transaction do
     organization_id: 2
   })
 
-  Animal.create!({
+  cat_8 = Animal.create!({
     species: "Cat",
     breed: "Domestic Short Hair",
     size: "Small",
@@ -205,7 +205,7 @@ ApplicationRecord.transaction do
     organization_id: 3
   })
 
-  Animal.create!({
+  cat_9 = Animal.create!({
     species: "Cat",
     breed: "Domestic Short Hair",
     size: "Small",
@@ -218,7 +218,7 @@ ApplicationRecord.transaction do
     organization_id: 3
   })
 
-  Animal.create!({
+  cat_10 = Animal.create!({
     species: "Cat",
     breed: "Domestic Short Hair Mix",
     size: "Medium",
@@ -232,7 +232,7 @@ ApplicationRecord.transaction do
     organization_id: 4
   })
 
-  Animal.create!({
+  cat_11 = Animal.create!({
     species: "Cat",
     breed: "Tabby",
     size: "Medium",
@@ -246,7 +246,7 @@ ApplicationRecord.transaction do
     organization_id: 5
   })
 
-  Animal.create!({
+  dog_1 = Animal.create!({
     species: "Dog",
     breed: "German Shepherd Dog & Smooth Collie Mix",
     size: "Medium",
@@ -260,7 +260,7 @@ ApplicationRecord.transaction do
     organization_id: 5
   })
 
-  Animal.create!({
+  dog_2 = Animal.create!({
     species: "Dog",
     breed: "Mixed Breed",
     size: "Medium",
@@ -273,7 +273,7 @@ ApplicationRecord.transaction do
     organization_id: 4
   })
 
-  Animal.create!({
+  dog_3 = Animal.create!({
     species: "Dog",
     breed: "Mixed Breed",
     size: "Medium",
@@ -287,7 +287,7 @@ ApplicationRecord.transaction do
     organization_id: 4
   })
 
-  Animal.create!({
+  dog_4 = Animal.create!({
     species: "Dog",
     breed: "Shepherd Mix",
     size: "Large",
@@ -301,7 +301,7 @@ ApplicationRecord.transaction do
     organization_id: 4
   })
 
-  Animal.create!({
+  dog_5 = Animal.create!({
     species: "Dog",
     breed: " Dutch Shepherd Mix",
     size: "Large",
@@ -315,7 +315,7 @@ ApplicationRecord.transaction do
     organization_id: 6
   })
 
-  Animal.create!({
+  dog_6 = Animal.create!({
     species: "Dog",
     breed: "Labrador Retriever & Shepherd Mix",
     size: "Medium",
@@ -329,7 +329,7 @@ ApplicationRecord.transaction do
     organization_id: 6
   })
 
-  Animal.create!({
+  dog_7 = Animal.create!({
     species: "Dog",
     breed: "Labrador Retriever Mix",
     size: "Large",
@@ -343,7 +343,7 @@ ApplicationRecord.transaction do
     organization_id: 6
   })
 
-  Animal.create!({
+  dog_8 = Animal.create!({
     species: "Dog",
     breed: "Cattle Dog & Australian Shepherd Mix",
     size: "Medium",
@@ -357,7 +357,7 @@ ApplicationRecord.transaction do
     organization_id: 7
   })
 
-  Animal.create!({
+  dog_9 = Animal.create!({
     species: "Dog",
     breed: "Australian Shepherd & Border Collie Mix ",
     size: "Medium",
@@ -371,7 +371,7 @@ ApplicationRecord.transaction do
     organization_id: 7
   })
 
-  Animal.create!({
+  dog_10 = Animal.create!({
     species: "Dog",
     breed: "Labrador Retriever Mix",
     size: "Medium",
@@ -385,7 +385,7 @@ ApplicationRecord.transaction do
     organization_id: 7
   })
 
-  Animal.create!({
+  dog_11 = Animal.create!({
     species: "Dog",
     breed: "Labrador Retriever Mix",
     size: "Medium",
@@ -399,7 +399,7 @@ ApplicationRecord.transaction do
     organization_id: 7
   })
 
-  Animal.create!({
+  dog_12 = Animal.create!({
     species: "Dog",
     breed: "Rat Terrier Mix",
     size: "Small",
@@ -413,7 +413,7 @@ ApplicationRecord.transaction do
     organization_id: 8
   })
 
-  Animal.create!({
+  dog_13 = Animal.create!({
     species: "Dog",
     breed: "Husky & German Shepherd Dog Mix",
     size: "Large",
@@ -427,7 +427,7 @@ ApplicationRecord.transaction do
     organization_id: 8
   })
 
-  Animal.create!({
+  dog_14 = Animal.create!({
     species: "Dog",
     breed: "Husky & German Shepherd Dog Mix",
     size: "Large",
@@ -441,7 +441,7 @@ ApplicationRecord.transaction do
     organization_id: 8
   })
 
-  Animal.create!({
+  dog_15 = Animal.create!({
     species: "Dog",
     breed: "Husky Mix",
     size: "Large",
@@ -455,5 +455,32 @@ ApplicationRecord.transaction do
     organization_id: 8
   })
 
+  puts "Attaching photos to animals..."
+  cat_1.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_1_chelsea.jpg"), filename: "cat_1_chelsea.jpg")
+  cat_2.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_2_harley.jpg"), filename: "cat_2_harley.jpg")
+  cat_3.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_3_kiwi.jpg"), filename: "cat_3_kiwi.jpg")
+  cat_4.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_4_jack.jpg"), filename: "cat_4_jack.jpg")
+  cat_5.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_5_sugar_plum.jpg"), filename: "cat_5_sugar_plum.jpg")
+  cat_6.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_6_cheetara.jpg"), filename: "cat_6_cheetara.jpg")
+  cat_7.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_7_peppermint.jpg"), filename: "cat_7_peppermint.jpg")
+  cat_8.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_8_babee.jpg"), filename: "cat_8_babee.jpg")
+  cat_9.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_9_treasure.jpg"), filename: "cat_9_treasure.jpg")
+  cat_10.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_10_joker.jpg"), filename: "cat_10_joker.jpg")
+  cat_11.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/cat_11_pablano.jpg"), filename: "cat_11_pablano.jpg")
+  dog_1.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_1_nikita.jpg"), filename: "dog_1_nikita.jpg")
+  dog_2.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_2_riri.jpg"), filename: "dog_2_riri.jpg")
+  dog_3.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_3_Julie.jpg"), filename: "dog_3_Julie.jpg")
+  dog_4.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_4_mollie.jpg"), filename: "dog_4_mollie.jpg")
+  dog_5.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_5_kenda.jpg"), filename: "dog_5_kenda.jpg")
+  dog_6.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_6_ryder.jpg"), filename: "dog_6_ryder.jpg")
+  dog_7.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_7_sally.jpg"), filename: "dog_7_sally.jpg")
+  dog_8.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_8_robin.jpg"), filename: "dog_8_robin.jpg")
+  dog_9.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_9_buttons.jpg"), filename: "dog_9_buttons.jpg")
+  dog_10.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_10_marley.jpg"), filename: "dog_10_marley.jpg")
+  dog_11.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_11_pax.jpg"), filename: "dog_11_pax.jpg")
+  dog_12.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_12_holly.jpg"), filename: "dog_12_holly.jpg")
+  dog_13.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_13_vetta.jpg"), filename: "dog_13_vetta.jpg")
+  dog_14.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_14_chevy.jpg"), filename: "dog_14_chevy.jpg")
+  dog_15.photos.attach(io: File.open("/home/chadfitzgerald/AppAcademy/aaa-Pawdar/images/dog_15_nova.jpg"), filename: "dog_15_nova.jpg")
   puts "Done!"
 end
