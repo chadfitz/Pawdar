cd362bc542cc65ea6d7fa73f3f2be323d47b4556
chad fitzgerald <fitzchad@gmail.com>
2023-02-03 18:19:25 -0800

    Change links to open in new tab

--- frontend/src/store/components/Footer/Footer.jsx
+++ frontend/src/store/components/Footer/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
     <div className='footer'>
       <h2>Created by Chad Fitzgerald</h2>
       <div className='footer-socials'>
-        <a href='https://github.com/chadfitz'><img className='footer-socials-link' src={github} alt="github link" /></a>
-        <a href="https://www.linkedin.com/in/chad-fitzgerald-956981ab/"><img className='footer-socials-link' src={linkedIn} alt="linkedIn link" /></a>
+        <a href='https://github.com/chadfitz' target="_blank"><img className='footer-socials-link' src={github} alt="github link" /></a>
+        <a href="https://www.linkedin.com/in/chad-fitzgerald-956981ab/" target="_blank"><img className='footer-socials-link' src={linkedIn} alt="linkedIn link" /></a>
       </div>
     </div>
   )
